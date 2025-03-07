7b668ff0ca5ad9894589a30d9400c468a5f1764c
A.Dmitriev <dmitandrew@gmail.com>
2025-03-07 13:53:01 +0300

    LCD(UART+DAC)
    меняется напряжение через uart, результат отображается на экране

--- interrupts.c
+++ interrupts.c
@@ -3,27 +3,62 @@
 #include <SEGGER_RTT.h>
 #include <SEGGER_RTT_Conf.h>
 #include <MDR32FxQI_dac.h>
+#include "LCD.h"
 
 void UART1_IRQHandler(void){
     if (UART_GetITStatusMasked(MDR_UART1, UART_IT_RX) == SET)
-    {
+    {		
+				LCD_Clear();
+				
         UART_ClearITPendingBit(MDR_UART1, UART_IT_RX);
 				DataByte = UART_ReceiveData(MDR_UART1);
 				SEGGER_RTT_printf(0,"DataByte: %d\n",DataByte);
 				lvl = DataByte;
-				SEGGER_RTT_printf(0,"lvl: %d\n",lvl);
 				DataByte = DataByte*819;
-				SEGGER_RTT_printf(0,"ChangedDataByte: %d\n",DataByte);
-				if (DataByte > 4095)
+				if (lvl==0)
 				{
-					DAC2_SetData(0);
-					UART_SendData(MDR_UART1, E);
-					UART_SendData(MDR_UART1, R);
-					UART_SendData(MDR_UART1, R);
-				}else
+					LCD_PrintLine(0, 0, 1, "LEVEL:0");
+					DAC2_SetData(DataByte);
+					UART_SendData(MDR_UART1, lvl);
+				}
+				else if(lvl==1)
 				{
+					LCD_PrintLine(0, 0, 1, "LEVEL:1");
 					DAC2_SetData(DataByte);
 					UART_SendData(MDR_UART1, lvl);	
 				}
+				else if(lvl==2)
+				{
+					LCD_PrintLine(0, 0, 1, "LEVEL:2");
+					DAC2_SetData(DataByte);
+					UART_SendData(MDR_UART1, lvl);	
+				}
+				else if(lvl==3)
+				{
+					LCD_PrintLine(0, 0, 1, "LEVEL:3");
+					DAC2_SetData(DataByte);
+					UART_SendData(MDR_UART1, lvl);	
+				}
+				else if(lvl==4)
+				{
+					LCD_PrintLine(0, 0, 1, "LEVEL:4");
+					DAC2_SetData(DataByte);
+					UART_SendData(MDR_UART1, lvl);	
+				}
+				else if(lvl==5)
+				{
+					LCD_PrintLine(0, 0, 1, "Level:5");
+					DAC2_SetData(DataByte);
+					UART_SendData(MDR_UART1, lvl);	
+				}
+				else if(lvl>5)
+				{
+					LCD_PrintLine(0, 0, 1, "ERROR");
+					DAC2_SetData(0);
+					UART_SendData(MDR_UART1, E);
+					UART_SendData(MDR_UART1, R);
+				}
+				SEGGER_RTT_printf(0,"lvl: %d\n",lvl);
+				SEGGER_RTT_printf(0,"ChangedDataByte: %d\n",DataByte);
     }	
 }
